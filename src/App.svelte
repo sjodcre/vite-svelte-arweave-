94ecbc8502c01e31ba7af0bf21ab94d409a2de34
Your Name <you@example.com>
2024-08-27 18:18:02 +0800

    final

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,22 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import {router, Route} from 'tinro'
+  let count = 0
+  router.mode.hash()
 </script>
 
-<main>
+<Route path="/">
+  <h1>Permaweb Counter</h1>
+  <div>Count: {count}</div>
   <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+    <button on:click={() => count++}>Inc </button>
   </div>
-  <h1>Vite + Svelte</h1>
+  <hr/>
+  <a href="/about">About</a>
+</Route>
 
-  <div class="card">
-    <Counter />
-  </div>
+<Route path="/about">
+  <h1>About Permaweb Counter</h1>
+  <a href="/">Go Home</a>
+</Route>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
+<!-- https://arweave.net/QcWY3ApiZz7RFzcTSnDTUrVQHBdnzSKtRuqu0a-ikzI -->
